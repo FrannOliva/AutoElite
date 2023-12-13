5a62bb06d26ae697cc24a63f89623cee3a59ec91
Francisco Oliva <franoliva845@gmail.com>
2023-12-13 13:09:49 -0300

    Cambios en ILC

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
     <>
       <StatusBar backgroundColor={colors.skyBlue} />
       <SafeAreaView style={styles.container}>
-        <Home />
+        <ItemListCategories />
       </SafeAreaView>
     </>
   );
--- src/Screens/ItemListCategories.js
+++ src/Screens/ItemListCategories.js
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Pressable, TextInput, View } from 'react-native'
+import Header from '../Components/Header'
+import { Ionicons } from '@expo/vector-icons'
+import { FontAwesome } from '@expo/vector-icons'
+import { colors } from '../Global/colors'
 
 const ItemListCategories = () => {
   return (
-    <View style={styles.container}>
-      <Text>ItemListCategories</Text>
-    </View>
+    <>
+      <Header title="PRODUCTOS" />
+      <View style={styles.container}>
+        <TextInput style={styles.input} placeholder='Buscar' placeholderTextColor={colors.black} />
+        <View style={styles.botones}>
+          <Pressable><Ionicons name="search" size={30} color={colors.black} /></Pressable>
+          <Pressable><FontAwesome name="close" size={30} color={colors.black} /></Pressable>
+        </View>
+      </View>
+    </>
+
   )
 }
 
@@ -12,5 +24,22 @@ export default ItemListCategories
 
 const styles = StyleSheet.create({
   container: {
+    width: "100%",
+    flexDirection: 'row',
+    paddingBottom: 10,
+    justifyContent: "center",
+    backgroundColor: colors.skyBlue
+  },
+  input: {
+    backgroundColor: colors.white,
+    width: "74%",
+    paddingLeft: 4,
+    borderColor: colors.black,
+    borderWidth: 2
+  },
+  botones: {
+    flexDirection: "row",
+    gap: 10,
+    marginLeft: 5
   }
 })
