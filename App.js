9be7a2b02f078682867b8f54dd5ec3afc441ab22
Francisco Oliva <franoliva845@gmail.com>
2023-12-20 13:49:28 -0300

    Pre-Navigation

--- App.js
+++ App.js
@@ -2,17 +2,15 @@ import { StyleSheet, SafeAreaView, StatusBar } from 'react-native'
 import { useState } from 'react'
 import { colors } from './src/Global/colors'
 import Home from './src/Screens/Home'
-import ItemListCategories from './src/Screens/ItemListCategories';
-import { useFonts } from 'expo-font';
-
-
+import ItemListCategories from './src/Screens/ItemListCategories'
+import { useFonts } from 'expo-font'
+import { fonts } from './src/Global/fonts'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 export default function App() {
   const [categorySelected, setCategorySelected] = useState()
-  const [fontLoaded] = useFonts({
-    ibmRegular: require("./assets/fonts/IBMPlexSans-Regular.ttf"),
-    ibmBold: require("./assets/fonts/IBMPlexSans-Bold.ttf")
-  })
+  const [fontLoaded] = useFonts(fonts)
 
   if (!fontLoaded) return null
 
---
+++ src/Global/fonts.js
@@ -0,0 +1,6 @@
+export const fonts = {
+  ibmRegular: require("../../assets/fonts/IBMPlexSans-Regular.ttf"),
+  ibmBold: require("../../assets/fonts/IBMPlexSans-Bold.ttf"),
+  ibmMono: require("../../assets/fonts/IBMPlexMono-Regular.ttf"),
+  ibmMonoBold: require("../../assets/fonts/IBMPlexMono-Bold.ttf"),
+}
